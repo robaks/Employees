03c2d6b80dd839f1aa1d7f9241ab1582e1101e2a
maxgu <maxgu@t4web.com.ua>
2015-02-24 08:27:08 +0200

    add codeception tests

---
+++ tests/unit/ServiceLocator/Controller/User/ShowControllerTest.php
@@ -0,0 +1,50 @@
+<?php
+namespace Employees\UnitTest\ServiceLocator\Controller\User;
+
+use Employees\Module;
+use Zend\ServiceManager\ServiceManager;
+use Zend\Mvc\Controller\ControllerManager;
+use Zend\ServiceManager\Config;
+use Zend\Mvc\Controller\PluginManager as ControllerPluginManager;
+use Zend\EventManager\EventManager;
+use Zend\EventManager\SharedEventManager;
+
+class ShowControllerTest extends \PHPUnit_Framework_TestCase
+{
+    private $serviceManager;
+
+    /**
+     * @var ControllerManager
+     */
+    private $controllerManager;
+
+    protected function setUp()
+    {
+        $module = new Module();
+
+        $events = new EventManager();
+        $sharedEvents = new SharedEventManager;
+        $events->setSharedManager($sharedEvents);
+
+        $plugins = new ControllerPluginManager();
+        $this->serviceManager = new ServiceManager();
+        $this->serviceManager->setService('Zend\ServiceManager\ServiceLocatorInterface', $this->serviceManager);
+        $this->serviceManager->setService('EventManager', $events);
+        $this->serviceManager->setService('SharedEventManager', $sharedEvents);
+        $this->serviceManager->setService('ControllerPluginManager', $plugins);
+
+        $this->controllerManager = new ControllerManager(new Config($module->getControllerConfig()));
+        $this->controllerManager->setServiceLocator($this->serviceManager);
+        $this->controllerManager->addPeeringServiceManager($this->serviceManager);
+    }
+
+    public function testCreation()
+    {
+        $this->assertTrue($this->controllerManager->has('Employees\Controller\User\Show'));
+
+        $controller = $this->controllerManager->get('Employees\Controller\User\Show');
+
+        $this->assertInstanceOf('Employees\Controller\User\ShowController', $controller);
+    }
+
+}
