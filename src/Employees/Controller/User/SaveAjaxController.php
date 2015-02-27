74301ba5d50a0dbe1c257a61a60dfcea61fbf075
maxgu <maxgu@t4web.com.ua>
2015-02-27 18:06:47 +0200

    draft save ajax

--- src/Employees/Controller/User/SaveAjaxController.php
+++ src/Employees/Controller/User/SaveAjaxController.php
@@ -7,8 +7,26 @@ use Zend\View\Model\JsonModel;
 
 class SaveAjaxController extends AbstractActionController {
 
+    public function __construct(SaveAjaxViewModel $view, $createService) {
+
+    }
+
     public function defaultAction()
     {
+        if (!$this->isPost()) {
+            return $this->view;
+        }
+
+        $params = $this->getFromPost();
+
+        $product = $this->createService->create($params);
+
+        if (!$product) {
+            $this->view->data = $params;
+            $this->view->error = $this->createService->getError();
+        }
+
+
         return new JsonModel();
     }
 
