39ec2d5019110bd29c410c1f79b0c6c38d548d1d
maxgu <maxgu@t4web.com.ua>
2015-03-06 16:57:47 +0200

    create employee ajax update

--- public/js/employees/add.js
+++ public/js/employees/add.js
@@ -44,15 +44,51 @@ $(function() {
                     });
                 }
 
-                $('#create-employee-btn').one('click', addEmployee);
-
-                if (response.formData['employee_id'] && ($('#add-employee .form-control[name=id]').length == 0)) {
+                if (response.formData['employeeId'] && ($('#add-employee .form-control[name=id]').length == 0)) {
                     $('#create-employee-btn').text('Update');
+                    $('#create-employee-btn').click(updateEmployee);
                     $('#add-employee').prepend('<input type="hidden" name="id" value="'+response.formData['employee_id']+'" class="form-control">');
                 }
             },
             error: function(response) {
                 $('#create-employee-btn').one('click', addEmployee);
+                console.log(response);
+            }
+        });
+
+        return false;
+    }
+
+    function updateEmployee() {
+        var data = {};
+        $("#add-employee form .form-control").each(function(i, obj) {
+            data[obj.name] = $(obj).val();
+        });
+
+        $.ajax({
+            url: '/employee/save',
+            type: 'POST',
+            data: data,
+            success: function(response) {
+                $("#add-employee form .form-control").each(function(i, obj) {
+                    var $parent = $(obj).closest('.form-group');
+                    $parent.removeClass('has-error');
+                    $parent.find('p.help-block').remove();
+                });
+
+                if (response.errors) {
+                    $.each(response.errors, function (field, errors) {
+                        var $parent = $('.form-control[name=' + field + ']').closest('.form-group');
+
+                        $parent.addClass('has-error');
+
+                        $.each(errors, function (key, error) {
+                            $parent.append('<p class="help-block">' + error + '</p>');
+                        });
+                    });
+                }
+            },
+            error: function(response) {
                 console.log(response);
             }
         });
