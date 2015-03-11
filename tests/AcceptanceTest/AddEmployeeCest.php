4aab0303c93dd9dade11fcaf0527eba47735d871
maxgu <maxgu@t4web.com.ua>
2015-03-11 18:44:30 +0200

    fix tests

--- tests/AcceptanceTest/AddEmployeeCest.php
+++ tests/AcceptanceTest/AddEmployeeCest.php
@@ -3,6 +3,18 @@ namespace EmployeesTest;
 
 class AddEmployeeCest
 {
+
+    public function _before(AcceptanceTester $I)
+    {
+        $I->amOnPage('/login-form');
+        $I->fillField('username', 'asd');
+        $I->fillField('password', '111111');
+        $I->click('SIGN IN');
+
+        $I->see('Welcome, John', '#menu-content-demo');
+        $I->seeInCurrentUrl('/');
+    }
+
     public function tryToTestAddEmployee(AcceptanceTester $I)
     {
         $I->wantTo('Add employee');
--- tests/AcceptanceTest/ListEmployeesCest.php
+++ tests/AcceptanceTest/ListEmployeesCest.php
@@ -3,6 +3,17 @@ namespace EmployeesTest;
 
 class ListEmployeesCest
 {
+    public function _before(AcceptanceTester $I)
+    {
+        $I->amOnPage('/login-form');
+        $I->fillField('username', 'asd');
+        $I->fillField('password', '111111');
+        $I->click('SIGN IN');
+
+        $I->see('Welcome, John', '#menu-content-demo');
+        $I->seeInCurrentUrl('/');
+    }
+
     public function tryToTestListEmployees(AcceptanceTester $I)
     {
         $I->wantTo('List employees');
